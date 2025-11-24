5655ceece5e547b91b9085111904429c044d961a
gajger30 <mihelcicfran219@gmail.com>
2025-11-24 10:21:13 +0100

    a

--- GuessTheNumberGame/index.js
+++ GuessTheNumberGame/index.js
@@ -18,10 +18,39 @@ updateAttempts();
 function checkNumber(event){
     event.preventDefault(); //ne osvježava stranicu
     let value=parseInt(input.value);
-    console.log(value);
+    //console.log(value);
+    if(isNaN(value) || value<1 || value>100){
+        msg.innerHTML="Unesi broj između 1 i 100";
+        msg.style.color="crimson";
+        return;
+    }
+
+    pokusaji++;
+    updateAttempts();
+
+    if(value === randBroj){
+        msg.innerHTML="Pogodak, broj je; "+ randBroj;
+        msg.style.color= "green";
+    }
+    else if(value<randBroj){
+         msg.innerHTML="Premalo";
+        msg.style.color= "orange";
+    }
+    else{
+        msg.innerHTML="Previše";
+        msg.style.color= "orange";
+    }
 }
 
 function newGame(){
+randBroj=rand();
+pokusaji=0;
+input.value="";
+msg.innerHTML="Nova igra";
+msg.style.color="black";
+updateAttempts();
+input.focus();
+
 
 }
 
