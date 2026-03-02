8ec25ffe566895be5112b21de6c641c7b5f9216b
ZdenkoGrudnjak <mihelcicfran219@gmail.com>
2026-03-02 10:24:20 +0100

    LMAOOO qrkod

---
+++ skola/index.js
@@ -0,0 +1,21 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+   {
+    type: "input",
+    name: "ime",
+    message:"Unesi ime",
+   },
+
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
